6517221150affc4a08973ca2e7da509a43c782a9
bonny rolly <artemkar1032@gmail.com>
2024-09-10 20:35:25 +0500

    added getindex function

--- src/funcs/getindex.c
+++ src/funcs/getindex.c
@@ -2,6 +2,12 @@
 
 int getindex(List *list, Item *item) {
     int i = 0;
+    Item *temp = list->head;
+
+    while (temp != item && temp) {
+        i++;
+        temp = item->next;
+    }
 
     return i;
 }
