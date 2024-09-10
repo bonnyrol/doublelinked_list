09ba140c73d7361f5381231ab1d9ad35bd93039b
bonny rolly <artemkar1032@gmail.com>
2024-09-10 20:15:31 +0500

    added add function

--- src/funcs/add.c
+++ src/funcs/add.c
@@ -1,5 +1,12 @@
 #include "../doublelinked_list.h"
 
 void add(List *list, Item *item) {
-    
+    if (!list->head) {
+        list->head = item, list->head = item;
+        item->next = NULL, item->prev = NULL; 
+    } else {
+        item->prev = list->tail;
+        item->next = NULL;
+        list->tail = item;
+    }
 }
